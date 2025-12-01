490a71f7f47ee0ed8134852f90e9832e9581c470
Diana Horbatiuk <diana@dianahorbatiuk.com>
2025-11-30 22:12:29 -0300

    tirar footer da telinha de interpretar

--- frontend/src/routes/interpretations/new.tsx
+++ frontend/src/routes/interpretations/new.tsx
@@ -2,7 +2,6 @@ import { createFileRoute } from '@tanstack/react-router'
 import { FormWrapper } from '../../components/reading/form/form.tsx'
 import InterpretationForm from '../../components/reading/form/InterpretationForm.tsx'
 import { useTranslation } from 'react-i18next'
-import { Footer } from '../../components/layout/Footer.tsx'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
@@ -23,7 +22,6 @@ function RouteComponent () {
         <h2>{t('reading.interpretation.title')}</h2>
         <InterpretationForm/>
       </FormWrapper>
-      <Footer/>
     </Wrapper>
   )
 }
