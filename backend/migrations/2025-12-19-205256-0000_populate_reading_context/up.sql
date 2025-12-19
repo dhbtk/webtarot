753d6c09d4294f039145cb70c62affa92764c3a1
Diana Horbatiuk <diana@dianahorbatiuk.com>
2025-12-19 18:35:54 -0300

    actually fix migration

--- backend/migrations/2025-12-19-205256-0000_populate_reading_context/up.sql
+++ backend/migrations/2025-12-19-205256-0000_populate_reading_context/up.sql
@@ -3,4 +3,5 @@
 update readings
 set question = (regexp_match(question, '(^.+?\?+)'))[1],
     context  = trim(regexp_replace(question, '(^.+?\?+)', ''))
-where context = '' and and (regexp_match(question, '(^.+?\?+)'))[1] is not null;
+where context = ''
+  and (regexp_match(question, '(^.+?\?+)'))[1] is not null;
